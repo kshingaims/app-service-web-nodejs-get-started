5b2f5796c83a712e9fd6f1a350c59515a17ddab1
kshingaims <103156183+kshingaims@users.noreply.github.com>
2023-04-02 12:37:41 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/wait', async (req, res) => {
+  const requestData = req.body;
+  await new Promise(resolve => setTimeout(resolve, 60 * 1000));
+  res.status(200).send('Waited for 3 minutes');
+});
+
 module.exports = router;
