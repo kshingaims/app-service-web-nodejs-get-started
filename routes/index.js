bb32e2c9ed4eebcbb5a45749d4f0c273e17774f8
kshingaims <103156183+kshingaims@users.noreply.github.com>
2023-04-02 12:44:19 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const os = require('os');
+
+const networkInterfaces = os.networkInterfaces();
+const ipv4Interfaces = networkInterfaces['Ethernet'].filter(interface => interface.family === 'IPv4');
+const ipv4Address = ipv4Interfaces[0].address;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,8 +13,8 @@ router.get('/', function(req, res, next) {
 
 router.post('/wait', async (req, res) => {
   const requestData = req.body;
-  await new Promise(resolve => setTimeout(resolve, 60 * 1000));
-  res.status(200).send('Waited for 3 minutes');
+  await new Promise(resolve => setTimeout(resolve, 3 * 60 * 1000));
+  res.status(200).send('Waited for 3 minutes: ' + ipv4Address);
 });
 
 module.exports = router;
