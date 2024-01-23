c54fe1e7bb7b7a0fb9b1037c971fa9fcc39a1573
kshingaims <103156183+kshingaims@users.noreply.github.com>
2024-01-23 15:23:50 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("--- test ---");
+  console.log("--- error ---")
   res.render('index', { title: 'Express' });
 });
 
